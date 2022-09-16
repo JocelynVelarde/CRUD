d09903c03eef1473e6603e30bf18572278ed8036
Jocelyn Velarde <a01285780@itesm.mx>
2022-09-16 10:13:53 -0500

    Se agrego la parte del Login, solo los nombres y contraseñas que se obtengan de la tabla user podran ser admitidos en el sistema. Tambien te permite navegar entre paginas para poder hacer primero el login, luego ver la home page y luego poder dar de alta a un nuevo usuario. Ya quedo todo el backend ahora falta el frontend. La pagina del login ya la tengo diseñada en otro proyecto asi que solo la traere hacia aca

---
+++ CRUD/Form2.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace CRUD
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.usuario = new System.Windows.Forms.TextBox();
+            this.contraseña = new System.Windows.Forms.TextBox();
+            this.btnLogIn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // usuario
+            // 
+            this.usuario.Location = new System.Drawing.Point(331, 165);
+            this.usuario.Name = "usuario";
+            this.usuario.Size = new System.Drawing.Size(100, 22);
+            this.usuario.TabIndex = 0;
+            // 
+            // contraseña
+            // 
+            this.contraseña.Location = new System.Drawing.Point(331, 221);
+            this.contraseña.Name = "contraseña";
+            this.contraseña.Size = new System.Drawing.Size(100, 22);
+            this.contraseña.TabIndex = 1;
+            // 
+            // btnLogIn
+            // 
+            this.btnLogIn.Location = new System.Drawing.Point(344, 310);
+            this.btnLogIn.Name = "btnLogIn";
+            this.btnLogIn.Size = new System.Drawing.Size(75, 23);
+            this.btnLogIn.TabIndex = 2;
+            this.btnLogIn.Text = "Log In";
+            this.btnLogIn.UseVisualStyleBackColor = true;
+            this.btnLogIn.Click += new System.EventHandler(this.btnLogIn_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnLogIn);
+            this.Controls.Add(this.contraseña);
+            this.Controls.Add(this.usuario);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox usuario;
+        private System.Windows.Forms.TextBox contraseña;
+        private System.Windows.Forms.Button btnLogIn;
+    }
+}
---
+++ CRUD/Form2.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SQLite;
+
+namespace CRUD
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogIn_Click(object sender, EventArgs e)
+        {
+            if (usuario.Text.Trim() == "" && contraseña.Text.Trim() == "")
+            {
+                MessageBox.Show("Campos Vacios", "Error");
+            }
+            else
+            {
+                string query = "SELECT * FROM usuario WHERE Nombre= @nombre AND Contraseña = @contraseña";
+                SQLiteConnection conn = new SQLiteConnection("Data Source=PruebaDB.db;Version=3;");  
+                conn.Open();
+                SQLiteCommand cmd = new SQLiteCommand(query, conn);
+                cmd.Parameters.AddWithValue("@nombre", usuario.Text);
+                cmd.Parameters.AddWithValue("@contraseña", contraseña.Text);
+                SQLiteDataAdapter adapter = new SQLiteDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                adapter.Fill(dt);
+
+                if(dt.Rows.Count > 0)
+                {
+                    MessageBox.Show("Login Existoso");
+                    Form3 form3 = new Form3();
+                    form3.ShowDialog();
+                }
+                else
+                {
+                    MessageBox.Show("Error intenta de nuevo");
+                }
+            }
+        }
+    }
+}
